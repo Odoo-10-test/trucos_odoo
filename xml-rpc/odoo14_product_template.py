ef1d1fbdfa4f8f4d7a1486748675b61add69c2a3
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-11-17 10:37:04 +0100

    sc

--- xml-rpc/odoo14_product_template.py
+++ xml-rpc/odoo14_product_template.py
@@ -22,6 +22,43 @@ object_proxy = xmlrpc.client.ServerProxy(url + 'object')
 
 uid = common_proxy.login(db, user, password)
 
+path_file = 'product.csv'
 
 if uid:
     print('Conectado al servidor maestro')
+
+
+def _create(estado):
+    if estado is True:
+        archive = csv.DictReader(open(path_file))
+        cont = 0
+        for field in archive:
+            print(field)
+            cont += 1
+            """1"""
+            default_code = field['default_code'].strip()
+            name = field['name'].strip()
+            barcode = field['barcode'].strip()
+
+            """2"""
+            product_find = object_proxy.execute(db,uid,password,'product.template','search',[('default_code','=',default_code)])
+            if product_find:
+                print("El producto existe")
+            else:
+                vals = {}
+                vals['barcode'] = barcode
+                vals['name'] = name
+                vals['default_code'] = default_code
+
+                product_id = object_proxy.execute(db,uid,password,'product.template','create',vals)
+                if product_id:
+                    print(cont,"INSERTADO PRODUCTO:", name)
+                else:
+                    print("Error")
+
+
+def main():
+    print("Ha comenzado el proceso")
+    _create(True)
+    print('Ha finalizado la carga tabla')
+main()
